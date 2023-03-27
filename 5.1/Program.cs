6cd0bcfca9cb2255e131d9c5113c0f12c463afed
Senka1 <40804722+Senka1@users.noreply.github.com>
2023-03-27 22:20:08 +0300

    Add files via upload

---
+++ 5.1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+interface IHello
+{
+    void SayHello();
+}
+
+class RussianHello : IHello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Привет!");
+    }
+}
+
+class EnglishHello : IHello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+}
+
+class UkrainHello : IHello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Прiвiт!");
+    }
+}
+
+class PolishHello : IHello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Cześć!");
+    }
+}
+
+class ItalianHello : IHello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Ciao!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<IHello> hellos = new List<IHello>()
+        {
+            new RussianHello(),
+            new EnglishHello(),
+            new UkrainHello(),
+            new PolishHello(),
+            new ItalianHello()
+        };
+
+        foreach (IHello hello in hellos)
+        {
+            Greet(hello);
+        }
+
+        Console.ReadKey();
+    }
+
+    static void Greet(IHello hello)
+    {
+        Console.Write("The greeting in your language is: ");
+        hello.SayHello();
+    }
+}
---
+++ 5/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+interface IAnimal
+{
+    void Voice();
+}
+
+class Frog : IAnimal
+{
+    public void Voice()
+    {
+        Console.WriteLine("Ква!");
+    }
+}
+
+class Dog : IAnimal
+{
+    public void Voice()
+    {
+        Console.WriteLine("Гав!");
+    }
+}
+
+class Cat : IAnimal
+{
+    public void Voice()
+    {
+        Console.WriteLine("Мяу!");
+    }
+}
+
+class Ram : IAnimal
+{
+    public void Voice()
+    {
+        Console.WriteLine("Мээ!");
+    }
+}
+
+class Sparrow : IAnimal
+{
+    public void Voice()
+    {
+        Console.WriteLine("Чирик!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<IAnimal> animals = new List<IAnimal>
+        {
+            new Frog(),
+            new Dog(),
+            new Cat(),
+            new Ram(),
+            new Sparrow()
+        };
+
+        foreach (IAnimal animal in animals)
+        {
+            PetAnimal(animal);
+        }
+
+        Console.ReadKey();
+    }
+
+    static void PetAnimal(IAnimal animal)
+    {
+        Console.WriteLine("Мы гладим зверушку, а она нам говорит:");
+        animal.Voice();
+    }
+}
