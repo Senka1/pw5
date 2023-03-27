d9edcef9ceb0d1dba605f746627a44980017f9cb
Senka1 <40804722+Senka1@users.noreply.github.com>
2023-03-27 22:21:14 +0300

    Add files via upload

---
+++ 5.3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+interface IFilter
+{
+    string Execute(string textLine);
+}
+
+class DigitFilter : IFilter
+{
+    public string Execute(string textLine)
+    {
+        string text = "";
+
+        foreach (char c in textLine)
+        {
+            if (char.IsLetter(c))
+            {
+                text += c;
+            }
+        }
+
+        return text;
+    }
+}
+
+class LetterFilter : IFilter
+{
+    public string Execute(string textLine)
+    {
+        string text = "";
+
+        foreach (char c in textLine)
+        {
+            if (char.IsDigit(c))
+            {
+                text += c;
+            }
+        }
+
+        return text;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IFilter digitFilter = new DigitFilter();
+        IFilter letterFilter = new LetterFilter();
+
+        string text = "dd4dhfw2";
+
+        Console.WriteLine($"Исходный текст: {text}");
+        Console.WriteLine($"Текст после применения DigitFilter: {digitFilter.Execute(text)}");
+        Console.WriteLine($"Текст после применения LetterFilter: {letterFilter.Execute(text)}");
+
+        Console.ReadKey();
+    }
+}
---
+++ 5.4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+interface IShape
+{
+    void Draw(int size);
+}
+
+class VerticalLine : IShape
+{
+    public void Draw(int size)
+    {
+        for (int i = 0; i < size; i++)
+            Console.WriteLine("|");
+    }
+}
+
+class HorizontalLine : IShape
+{
+    public void Draw(int size)
+    {
+        for (int i = 0; i < size; i++)
+            Console.Write("-");
+        Console.WriteLine();
+    }
+}
+
+class Square : IShape
+{
+    public void Draw(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+                Console.Write("[]");
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IShape shape;
+
+        shape = new VerticalLine();
+        shape.Draw(5);
+
+        shape = new HorizontalLine();
+        shape.Draw(5);
+
+        shape = new Square();
+        shape.Draw(5);
+
+        Console.ReadKey();
+    }
+}
